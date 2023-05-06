1cab4d17b66c5ddeb8cfd7eb63269743b624d7c0
st3phano <stephano-@hotmail.com>
2023-05-05 21:33:31 -0300

    fixed docstring formatting

--- PBL04/exercicio01.py
+++ PBL04/exercicio01.py
@@ -2,8 +2,8 @@
 Faça um programa que possua um vetor que armazene 6 números
 inteiros. O programa deve executar os seguintes passos:
 a. Armazene os seguintes valores ao vetor: 1, 0, 5, -2, -5, 7.
-b. Armazene em uma variável a soma dos valores nas posições 0, 1
-e 5. Imprima na tela a soma.
+b. Armazene em uma variável a soma dos valores nas posições 0, 1 e 5.
+   Imprima na tela a soma.
 c. Modifique o valor da posição 4 para 100.
 d. Imprima na tela cada valor do vetor, um em cada linha.
 '''

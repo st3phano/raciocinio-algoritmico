9512a81def6b8899da587de5b39c76035b762338
st3phano <stephano-@hotmail.com>
2023-05-18 10:52:59 -0300

    optimize algorithm

--- PBL05/exercicio10.py
+++ PBL05/exercicio10.py
@@ -4,17 +4,27 @@ Dado um vetor = [2, 4, 7, 2, 3, 3, 1, 0, 2, 6] imprima o número que se repete m
 
 inteiros = [2, 4, 7, 2, 3, 3, 1, 0, 2, 6]
 
-inteiroMaisRepetido = 0
-repeticoesInteiroMaisRepetido = 0
-for i in range(len(inteiros)):
-   inteiro = inteiros[i]
-   repeticoesInteiro = 1
-   for j in range(i + 1, len(inteiros)):
-      if (inteiros[j] == inteiro):
-         repeticoesInteiro += 1
+repeticoesPorInteiro = [[0, 0] for _ in range(len(inteiros))] # [inteiro, repetições]
+repeticoesPorInteiro[0][0] = inteiros[0]
+repeticoesPorInteiro[0][1] += 1
+tamanhoRepeticoesPorInteiro = 1
+inteiroMaisRepetido = repeticoesPorInteiro[0][0] # primeiro inteiro do vetor
+repeticoesInteiroMaisRepetido = repeticoesPorInteiro[0][1] # repetições primeiro inteiro do vetor
+for i in range(1, len(inteiros)):
+   inteiroRegistrado = False
+   for j in range(tamanhoRepeticoesPorInteiro):
+      if (inteiros[i] == repeticoesPorInteiro[j][0]):
+         repeticoesPorInteiro[j][1] += 1
+         inteiroRegistrado = True
+         break
 
-   if (repeticoesInteiro > repeticoesInteiroMaisRepetido):
-      repeticoesInteiroMaisRepetido = repeticoesInteiro
-      inteiroMaisRepetido = inteiro
+   if (not inteiroRegistrado):
+      repeticoesPorInteiro[tamanhoRepeticoesPorInteiro][0] = inteiros[i]
+      repeticoesPorInteiro[tamanhoRepeticoesPorInteiro][1] += 1
+      tamanhoRepeticoesPorInteiro += 1
+
+   if (repeticoesPorInteiro[i][1] > repeticoesInteiroMaisRepetido):
+      inteiroMaisRepetido = repeticoesPorInteiro[i][0]
+      repeticoesInteiroMaisRepetido = repeticoesPorInteiro[i][1]
 
 print(f"Inteiro que se repete mais vezes: {inteiroMaisRepetido}")

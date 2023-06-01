d728ddaf8b9b484635cc7e8a3b71590873ba7e9a
st3phano <stephano-@hotmail.com>
2023-06-01 11:05:00 -0300

    first version

---
+++ semana14/exercicio03.py
@@ -0,0 +1,9 @@
+'''
+Escreva uma função chamada "criar_vetor" que retorna um vetor preenchido com zeros
+de tamanho 5. 
+'''
+
+def criarVetor(tamanho: int) -> list[int]:
+   return [0] * tamanho
+
+print(criarVetor(5))
